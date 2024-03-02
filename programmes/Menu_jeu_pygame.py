2a115f58343232b3e2e25c9442a97321b62a743f
Corentin Finou <corentin.finou@gmail.com>
2024-03-02 22:50:06 +0100

    Gestion par app.py

--- programmes/Menu_jeu_pygame.py
+++ programmes/Menu_jeu_pygame.py
@@ -1,73 +1,75 @@
 import pygame
 import os
 
-#import images avec noms correspondant aux noms donnés par paul aux variables du programme
-from consts import imageFondForet as fond_menu, bouton_jouer_img,bouton_quitter_img 
+def menu():
+    #import images avec noms correspondant aux noms donnés par paul aux variables du programme
+    from consts import imageFondForet as fond_menu, bouton_jouer_img,bouton_quitter_img 
 
-# Initialisation de Pygame
-pygame.init()
+    # Initialisation de Pygame
+    pygame.init()
 
-# Paramètres de la fenêtre
-fenetre = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-pygame.display.set_caption("Menu")
+    # Paramètres de la fenêtre
+    fenetre = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("Menu")
 
-# Redimensionnement des images
-fond_menu = pygame.transform.scale(fond_menu, fenetre.get_size())
-bouton_jouer_img = pygame.transform.scale(bouton_jouer_img, (300, 150))
-bouton_quitter_img = pygame.transform.scale(bouton_quitter_img, (300, 150))
+    # Redimensionnement des images
+    fond_menu = pygame.transform.scale(fond_menu, fenetre.get_size())
+    bouton_jouer_img = pygame.transform.scale(bouton_jouer_img, (300, 150))
+    bouton_quitter_img = pygame.transform.scale(bouton_quitter_img, (300, 150))
 
-# Police de caractères
-police = pygame.font.Font(None, 40)
+    # Police de caractères
+    police = pygame.font.Font(None, 40)
 
-# Classe pour les boutons
-class Bouton:
-    def __init__(self, x, y, image, action):
-        self.rect = image.get_rect()
-        self.rect.topleft = (x, y)
-        self.image = image
-        self.action = action
+    # Classe pour les boutons
+    class Bouton:
+        def __init__(self, x, y, image, action):
+            self.rect = image.get_rect()
+            self.rect.topleft = (x, y)
+            self.image = image
+            self.action = action
+            self.state = False
 
-    def dessiner(self):
-        fenetre.blit(self.image, self.rect)
+        def dessiner(self):
+            fenetre.blit(self.image, self.rect)
 
-    def verifier_clic(self, pos):
-        if self.rect.collidepoint(pos):
-            self.action()
+        def verifier_clic(self, pos):
+            if self.rect.collidepoint(pos):
+                self.action(self)
 
-# Fonctions pour les actions des boutons
-def action_jouer():
-    os.system("python ../programmes_prototypes/programmeTestGraphique_.py")
-    action_quitter()
+    # Fonctions pour les actions des boutons
+    def action_jouer(self):
+        self.state = True
 
-def action_quitter():
-    pygame.quit()
-    quit()
+    def action_quitter(self):
+        pygame.quit()
+        quit()
 
-# Création des boutons
-bouton_jouer = Bouton(550, 300, bouton_jouer_img, action_jouer)
-bouton_quitter = Bouton(550, 400, bouton_quitter_img, action_quitter)
-boutons = [bouton_jouer, bouton_quitter]
+    # Création des boutons
+    bouton_jouer = Bouton(550, 300, bouton_jouer_img, action_jouer)
+    bouton_quitter = Bouton(550, 400, bouton_quitter_img, action_quitter)
+    boutons = [bouton_jouer, bouton_quitter]
 
-# Boucle principale
-en_cours = True
-while en_cours:
-    fenetre.blit(fond_menu, (0, 0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            en_cours = False
-        if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    action_quitter()      
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                pos = pygame.mouse.get_pos()
-                for bouton in boutons:
-                    bouton.verifier_clic(pos)
+    # Boucle principale
+    def lancer():
+        en_cours = True
+        while en_cours:
+            en_cours = True if bouton_jouer.state == False else False
+            fenetre.blit(fond_menu, (0, 0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    en_cours = False
+                if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            en_cours = False   
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        pos = pygame.mouse.get_pos()
+                        for bouton in boutons:
+                            bouton.verifier_clic(pos)
 
-    for bouton in boutons:
-        bouton.dessiner()
 
-    pygame.display.flip()
+            for bouton in boutons:
+                bouton.dessiner()
 
-pygame.quit()
-quit()
+            pygame.display.flip()
+    lancer()
---
+++ programmes/app.py
@@ -0,0 +1,7 @@
+from programmeTestGraphique_ import jeu
+from Menu_jeu_pygame import menu
+
+lancerAvecMenu = False #a modif si besoin
+if lancerAvecMenu:
+    menu()
+jeu()
