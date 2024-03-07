45c1d69d7e36d120ddac7f2c3110cce3e98397d3
Corentin Lordi <corentin.finou@gmail.com>
2024-03-07 09:10:25 +0100

    readme

--- programmes/Menu_jeu_pygame.py
+++ programmes/Menu_jeu_pygame.py
@@ -40,7 +40,7 @@ def menu():
     def action_jouer(self):
         self.state = True
 
-    def action_quitter(self):
+    def action_quitter():
         pygame.quit()
         quit()
 
--- programmes/app.py
+++ programmes/app.py
@@ -1,7 +1,7 @@
 from programmeTestGraphique_ import jeu
 from Menu_jeu_pygame import menu
 
-lancerAvecMenu = False #a modif si besoin
+lancerAvecMenu = True #a modif si besoin
 if lancerAvecMenu:
     menu()
 jeu()
