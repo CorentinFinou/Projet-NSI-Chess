f4d314cf76feca0223f78fd2e21b93c78ed6dce4
Corentin Finou <corentin.finou@gmail.com>
2024-03-24 15:09:01 +0100

    Changements fichiers

---
+++ Projet-NSI-Chess-cartes/doc/readme.md
@@ -0,0 +1,2 @@
+Si vous êtes sur une machine virtuelle et/ou que pygame n'est pas installé :
+    dans le terminal : $ pip install pygame
--- Projet-NSI-Chess-cartes/sources/scripts/app.py
+++ Projet-NSI-Chess-cartes/sources/scripts/app.py
@@ -1,7 +1,6 @@
 from jeu import jeu
 from menu import menu
 from Sound import Sound
-#from musique import musique
 
 lancerAvecMenu = True #a modif si besoin
 musique = True
